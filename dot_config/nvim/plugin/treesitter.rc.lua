0ef178f9bcd0bd292273398ba3cd97afb8a1b20d
hokupod <hokupod@outlook.com>
2023-12-31 08:48:07 +0900

    update treesitter settings.

--- dot_config/nvim/plugin/treesitter.rc.lua
+++ dot_config/nvim/plugin/treesitter.rc.lua
@@ -2,7 +2,7 @@ local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if (not status) then return end
 
 treesitter.setup {
-  ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "svelte", "html", "ruby", "go", "zig", },
+  ensure_installed = { "lua", "vim", "vimdoc", "javascript", "typescript", "svelte", "html", "ruby", "go", "zig", "markdown", "markdown_inline" },
   highlight = {
     enable = true, -- ハイライトを有効化
     additional_vim_regex_highlighting = false, -- catpuucin用
